8266f6810c0053a563d5a799f6a12b7be00964e6
Paul Robinson <paul.d.robinson@gmail.com>
2025-09-22 13:33:04 +0000

    added struc read write

--- espnow_test.py
+++ espnow_test.py
@@ -1,8 +1,19 @@
 from ESPythoNOW import *
+import struct
+
 
 def callback(from_mac, to_mac, msg):
   print("ESP-NOW message from %s to %s: %s" % (from_mac, to_mac, msg))
+  print(struct.unpack('f', msg))
 
-espnow = ESPythoNow(interface="wlp113s0", accept_all=True, callback=callback)
+espnow = ESPythoNow(interface="wlx984827db7610", accept_all=True, callback=callback)
+print(espnow)
 espnow.start()
+print(espnow)
+
+import time
+while True:
+   msg=struct.pack("f", (time.time() % 1.0)*25)
+   espnow.send("FF:FF:FF:FF:FF:FF", msg)
+   time.sleep(0.01)
 input() # Run until enter is pressed
