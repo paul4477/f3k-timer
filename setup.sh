ecdd121a8caff4e8608a6e3023fe6929a9815d1a
Paul Robinson <paul.d.robinson@gmail.com>
2025-09-28 09:35:33 +0100

    actual contents!

--- setup.sh
+++ setup.sh
@@ -2,6 +2,29 @@
 #
 #
 echo Setup...
-make venv
-install packages
-do setcap
+
+## create python venv
+python -m venv .venv
+source .venv/bin/activate
+
+pip install -r requirements.txt
+
+sudo setcap 'cap_net_bind_service=+ep cap_net_raw=+ep' .venv/bin/python
+
+## Set Master audio control to full volume
+amixer sset Master,0 "100%"
+DIR=`pwd`
+cat >start.sh <<EOL
+#!/bin/bash
+#
+#
+#
+ESPNOW_DEV=wlan1
+CHANNEL=4
+
+cd $DIR
+source .venv/bin/activate
+./prep.sh $ESPNOW_DEV $CHANNEL
+
+python f3k_timer.py
+EOL
