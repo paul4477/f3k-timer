7de6cc6c54b65fa880ad24663a7740e647022b9e
Paul Robinson <paul.d.robinson@gmail.com>
2025-10-03 21:22:49 +0100

    shortened some descriptions and changed window to time in most places

--- task_data.py
+++ task_data.py
@@ -2,106 +2,106 @@ f3k_task_timing_data = {}
 
 f3k_task_timing_data["f3k_a"] = {
             'name': "F3K Task A - Last 1 x 5:00 in 7:00",
-            'description': "F3k Task A: Last flight: Max five minutes: Seven minute working window.",
+            'description': "F3k Task A: Last flight: Max five minutes: Seven minute working time.",
             'windowTime': 420
 }
 f3k_task_timing_data["f3k_a2"] = {
             'name': "F3K Task A - Last 1 x 5:00 in 10:00",
-            'description': "F3k Task A: Last Flight. Max five minutes. Ten minute working window.",
+            'description': "F3k Task A: Last Flight. Max five minutes. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_b"] = {
             'name': "F3K Task B - Last 2 x 4:00",
-            'description': "F3k Task B: Last two flights. Max four minutes. Ten minute working window.",
+            'description': "F3k Task B: Last two flights. Max four minutes. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_b2"] = {
             'name': "F3K Task B - Last 2 x 3:00",
-            'description': "F3k Task B: Last two flights. Max three minutes. Seven minute working window.",
+            'description': "F3k Task B: Last two flights. Max three minutes. Seven minute working time.",
             'windowTime': 420
 }
 f3k_task_timing_data["f3k_c"] = {
             'name': "F3K Task C - All Up 3 x 3:00",
-            'description': "F3k Task C: All up last down. Single launch. Max three minutes. Three three minute working windows. Must launch before three second start signal ends.",
+            'description': "F3k Task C: All up last down. Single launch. Max three minutes. Three three minute working windows.",
             'windowTime': 183
 }
 f3k_task_timing_data["f3k_c2"] = {
             'name': "F3K Task C - All Up 4 x 3:00",
-            'description': "F3k Task C: All up last down. Single launch. Max three minutes. Four three minute working windows. Must launch before three second start signal ends.",
+            'description': "F3k Task C: All up last down. Single launch. Max three minutes. Four three minute working windows.",
             'windowTime': 183
 }
 f3k_task_timing_data["f3k_c3"] = {
             'name': "F3K Task C - All Up 5 x 3:00",
-            'description': "F3k Task C: All up last down. Single launch. Max three minutes. Five three minute working windows. Must launch before three second start signal ends.",
+            'description': "F3k Task C: All up last down. Single launch. Max three minutes. Five three minute working windows.",
             'windowTime': 183
 }
 f3k_task_timing_data["f3k_d"] = {
             'name': "F3K Task D - Ladder",
-            'description': "F3k Task D: Ladder. Increasing time by fifteen seconds. Flights of 30 seconds, 45 seconds, 1 minute, 1 minute 15 seconds, 1 minute 30 seconds, 1 minute 45 seconds, and 2 minutes. Ten minute working window. Must achieve or exceed flight time before proceeding to the next target flight.",
+            'description': "F3k Task D: Ladder. Increasing time by fifteen seconds. Flights of 30 seconds, 45 seconds, 1 minute, 1 minute 15 seconds, 1 minute 30 seconds, 1 minute 45 seconds, and 2 minutes. Ten minute working time. Must achieve or exceed flight time before proceeding to the next target flight.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_d2"] = {
             'name': "F3K Task D (2020) - 2 x 5:00",
-            'description': "F3k Task D: Two flights only. Max five minutes. Ten minute working window.",
+            'description': "F3k Task D: Two flights only. Max five minutes. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_e"] = {
             'name': "F3K Task E - Poker",
-            'description': "F3k Task E: Poker. Pilot nominated times. Five max target times. Ten minute working window. Must achieve or exceed flight time before proceeding to the next target flight.",
+            'description': "F3k Task E: Poker. Pilot nominated times. Five max target times. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_e2"] = {
             'name': "F3K Task E - Poker (2020) 10",
-            'description': "F3k Task E: Poker. Pilot nominated times. Three max target times. Ten minute working window. Must achieve or exceed flight time before proceeding to the next target flight. May call end of window.",
+            'description': "F3k Task E: Poker. Pilot nominated times. Three max target times. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_e3"] = {
             'name': "F3K Task E - Poker (2020) 15",
-            'description': "F3k Task E: Poker. Pilot nominated times. Three max target times. Fifteen minute working window. Must achieve or exceed flight time before proceeding to the next target flight. May call end of window.",
+            'description': "F3k Task E: Poker. Pilot nominated times. Three max target times. Fifteen minute working time.",
             'windowTime': 900
 }
 f3k_task_timing_data["f3k_f"] = {
             'name': "F3K Task F - Best 3 x 3:00",
-            'description': "F3k Task F: Best three flights. Max three minutes. Maximum six launches. Ten minute working window.",
+            'description': "F3k Task F: Best three flights. Max three minutes. Maximum six launches. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_g"] = {
             'name': "F3K Task G - Best 5 x 2:00",
-            'description': "F3k Task G: Best five flights. Max two minutes. Ten minute working window.",
+            'description': "F3k Task G: Best five flights. Max two minutes. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_h"] = {
             'name': "F3K Task H - 1, 2, 3, 4",
-            'description': "F3k Task H: One minute, two minute, three minute and four minute max flights in any order. Ten minute working window.",
+            'description': "F3k Task H: One minute, two minute, three minute and four minute max flights in any order. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_i"] = {
             'name': "F3K Task I - 3 x 3:20",
-            'description': "F3k Task I: Three longest flights. Max three minutes twenty seconds. Ten minute working window.",
+            'description': "F3k Task I: Three longest flights. Max three minutes twenty seconds. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_j"] = {
             'name': "F3K Task J - Last 3 x 3:00",
-            'description': "F3k Task J: Last three flights. Max three minutes. Ten minute working window.",
+            'description': "F3k Task J: Last three flights. Max three minutes. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_k"] = {
             'name': "F3K Task K - Big Ladder",
-            'description': "F3k Task K: Big Ladder. Five target flights increasing time by thirty seconds. One minute, one minute thirty seconds, two minutes, two minutes thirty seconds, and three minute flights in order. All time counts. Ten minute working window.",
+            'description': "F3k Task K: Big Ladder. Five target flights increasing time by thirty seconds. One minute, one minute thirty seconds, two minutes, two minutes thirty seconds, and three minute flights in order. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_l"] = {
             'name': "F3K Task L - Single Flight",
-            'description': "F3k Task L: Single flight. Single launch. Max time nine minutes fifty nine seconds. Ten minute working window.",
+            'description': "F3k Task L: Single flight. Single launch. Max time nine minutes fifty nine seconds. Ten minute working time.",
             'windowTime': 600
 }
 f3k_task_timing_data["f3k_m"] = {
             'name': "F3K Task M - Huge Ladder 3, 5, 7",
-            'description': "F3k Task M: Huge ladder. Three flights only. Three minute, five minute, and seven minnit flights in order. All time achieved counts. Fifteen minute working window.",
+            'description': "F3k Task M: Huge ladder. Three flights only. Three minute, five minute, and seven minnit flights in order. Fifteen minute working time.",
             'windowTime': 900
 }
 f3k_task_timing_data["f3k_n"] = {
             'name': "F3K Task N - Best Flight (9:59 Max)",
-            'description': "F3k Task N: Best flight. One flight only. Ten minute working window.",
+            'description': "F3k Task N: Best flight. One flight only. Ten minute working time.",
             'windowTime': 600
 }
